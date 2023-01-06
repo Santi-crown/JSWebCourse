74505f1197edc3cbbaf023b834fe2bf0b217d8c2
Santi-crown <santigomusik@gmail.com>
2023-01-06 06:48:15 -0500

    3. use strict mode

---
+++ 3.the-modern-mode/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// we need, in stric mode, to declare de variables
+let person;
+let haiblity;
+
+function hero (){
+    person = 'Santi God';
+    haiblity = 'Fly and be pretty';   
+console.log('Name:' + person + 'and his habilities are: ' + haiblity) ;
+};
+
+
+hero();
+
