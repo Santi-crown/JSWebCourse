d486f5c2843639aeb9c6e1b41752a5cc5901a583
Santi-crown <santigomusik@gmail.com>
2023-01-11 14:52:55 -0500

    6.interactions

---
+++ 2.Fundamentals/6.Interactions/main.js
@@ -0,0 +1,28 @@
+// ======> Interaction: alert, prompt, confirm
+
+//====> alert
+// alert('Hello');
+//The modal window prevents us from reading or interacting with the rest of the page until we press the window's ok button
+
+//====> prompt 
+//The function prompt accepts two arguments
+
+//result = prompt(title, [default]);
+
+// let age = prompt('Type your age', 100);
+
+// alert(`You are ${age} years old`);
+
+// console.log(typeof age);
+
+//====> confirm
+// sintax: result = confirm(question);
+
+//This function returns a booleans answer.
+
+// let isBoss = confirm('Are you the boss?');
+
+// alert( isBoss );
+
+
+
---
+++ 2.Fundamentals/6.Interactions/tasks.js
@@ -0,0 +1,3 @@
+let name = prompt('Name', 'Please type your name');
+
+alert(`Your name is ${name}`);
